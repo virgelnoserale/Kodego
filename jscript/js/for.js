fe38cd032fd9c751bc65322d582515c4747e461f
virgelnoserale <jhanhanes@gmail.com>
2022-11-09 16:05:46 +0800

    jvscript

---
+++ jscript/js/for.js
@@ -0,0 +1,12 @@
+const grocery =["Sugar", "Salt", "Rice", "Toyo", "Egg", "Vinegar", "Chicken", "Pork", "Beef", "CornedBeef", "Magic Sarap", "Fish", "Noodles", "Diswashing", "Soap", "Milk", "Coffee", "Milo", "Fruits", "Mineral Water"];
+    
+
+for (let i = 0; i < grocery.length; i++) {
+  let groceryLi = document.createElement('li');
+  groceryLi.innerText=grocery[i];
+  document.getElementById("list-groceries").appendChild(groceryLi);
+}
+
+console.log("---------------------------------------");
+console.log("Connection testing");
+console.log("---------------------------------------");
